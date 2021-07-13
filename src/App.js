297a8838cc7316185c4d33ad486e5ab3905e2f7a
Glaysonvisgueira <glaysonwow@gmail.com>
2021-07-13 14:32:40 -0300

    Novo layout

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ import { HomePage } from "./pages/HomePage";
 import { CMdashboard } from "./pages/CMdashboard";
 import { FaseDoisDashboard } from "./pages/FaseDoisDashboard";
 
+import { DashboardTeste } from "./pages/DashboardTeste";
+
 function App() {
   return (
     <BrowserRouter>     
         <Switch>
           <Route path="/homepage" exact component={HomePage} />
           <Route path="/" exact component={CMdashboard} />
+          <Route path="/teste" exact component={DashboardTeste} />
           <Route path="/geral" exact component={FaseDoisDashboard} />         
         </Switch>      
   </BrowserRouter>
